b8ad580acf685b3c6650511dfc7977705712871a
Sebastien Leib <sebastien.leib1@gmail.com>
2026-05-30 00:30:27 +0800

    Remove hardcoded base from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/etoile/',
   plugins: [react(), tailwindcss()],
 });
